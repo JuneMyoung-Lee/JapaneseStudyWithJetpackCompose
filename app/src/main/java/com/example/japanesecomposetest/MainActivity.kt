5261e6646efc230e2bd315ead7cd09b8313d3661
junemyounglee <june5215@naver.com>
2024-04-21 15:19:06 +0900

    build.gradle implementation 추가 및 NavigationBar 생성

--- app/src/main/java/com/example/japanesecomposetest/MainActivity.kt
+++ app/src/main/java/com/example/japanesecomposetest/MainActivity.kt
@@ -4,11 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AutoFixHigh
+import androidx.compose.material.icons.filled.Cookie
+import androidx.compose.material.icons.filled.Spa
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.japanesecomposetest.ui.theme.JapaneseComposeTestTheme
 
@@ -18,14 +32,76 @@ class MainActivity : ComponentActivity() {
         setContent {
             JapaneseComposeTestTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                    containerColor = MaterialTheme.colorScheme.background,
+                    bottomBar = {
+                        TestNavigationBar()
+                    }
+                ) { padding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(padding)
+                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+fun TestNavigationBar(modifier: Modifier = Modifier) {
+    val selectPosition = remember { mutableIntStateOf(0) }
+
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        modifier = modifier
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Cookie,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(id = R.string.navigation_item_title_1)
+                )
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(id = R.string.navigation_item_title_2)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AutoFixHigh,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(id = R.string.navigation_item_title_3)
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
     }
 }
 
@@ -41,6 +117,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JapaneseComposeTestTheme {
-        Greeting("Android")
+        TestNavigationBar()
     }
 }
